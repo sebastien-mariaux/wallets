18dee99dec7f046160214d43e60899ef6bc5686a
Sébastien Mariaux <sebastien.mariaux@protonmail.com>
2021-11-20 01:20:11 +0000

    wip update quantity value

--- app/javascript/controllers/coin_wallet_form_controller.js
+++ app/javascript/controllers/coin_wallet_form_controller.js
@@ -13,4 +13,24 @@ export default class extends Controller {
       .then(response => response.text())
       .then(html => this.quantityTarget.innerHTML = html)
   }
+
+  displayForm() {
+    this.quantityInput().type = 'number'
+    this.quantityInput().value = this.quantityValue()
+    this.element.querySelector(".quantity-value").classList.add('hidden')
+    this.quantityInput().focus()
+  }
+
+  quantityInput() {
+    return this.element.querySelector("input[name='quantity']")
+  }
+
+  updateValue() {
+    let newValue = this.quantityInput().value
+  }
+
+  quantityValue() {
+    let rawValue = this.quantityTarget.innerHTML
+    return parseFloat(rawValue) || 0
+  }
 }
